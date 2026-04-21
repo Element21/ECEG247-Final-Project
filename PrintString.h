e182199d984e3719603b9a01029017705617a177
aam027 <aam027@bucknell.edu>
2026-04-21 09:08:07 -0400

    Remove my LCD library

--- PrintString.h
+++
@@ -1,3 +0,0 @@
-#pragma once // Header guard
-
-void printString(char* text, unsigned int delay_ms);
--- main.c
+++ main.c
@@ -1,20 +1,51 @@
 #include <msp430.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include "InitLCD.h"
+#include "PrintString.h"
+
+void scrollString(char[] inputString) {
+
+}
+
+void showSplashScreen() {
+    initLCD();
+
+    printString("Reaction Time Test");
+}
+
+void debouceDelay() {
+    __delay_cycles(50000); // 50ms debounce
+}
 
 void waitForButton() {
   while (true) {
     if (BIT2 & P2IN) {
-      // Button pressed, continue execution
-      break
+        debouceDelay();
+        // Button pressed, continue execution
+        break
     }
   }
 }
 
-int main(void)
-{
+// Global vars (state machine)
+volatile unsigned long time_count = 0; // Count in 0.1ms (100us) increments
+volatile bool running = false;
+int random_seed = 0; // On start button pushed, grab timer value to seed RNG
+
+int main(void) {
     WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
+    
+    // 1MHz clock
+    DCOCTL = CALDCO_1MHZ;
+    BCSCTL1 = CALBC1_1MHZ;
 
     P1DIR |= 0x01;                            // Set P1.0 to output direction
-
-    initLCD();
 }
+
+// Timer A0 interrupt service routine
+#pragma vector = TIMER0_A0_VECTOR
+__interrupt void Timer_A(void) {
+    if (running) {
+        time_count++;
+    }
